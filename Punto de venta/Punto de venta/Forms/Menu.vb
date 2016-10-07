9e723f34c044b07cc01c590855b985c45537dd95
Josue <Josue@JOSUE-PC>
2016-10-07 00:42:33 -0500

    ciudades ya esta solo tiene 2 errores que no supe como solucionar, tambien el menu ya esta listo, solo falta colonias

---
+++ Punto de venta/Punto de venta/Forms/Menu.vb
@@ -0,0 +1,9 @@
+﻿Public Class Menu
+    Private Sub MantenimientoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles MantenimientoToolStripMenuItem.Click
+        FrmPais.Show()
+    End Sub
+
+    Private Sub SalirToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SalirToolStripMenuItem.Click
+        FrmPais.Close()
+    End Sub
+End Class
